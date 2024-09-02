63c2cb9a3749ee1324344e6367e4dd2578a11e3f
madDevXD <www.goyboe.com@gmail.com>
2024-09-02 15:11:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = 'Hello, Flet!'
-    page.add(ft.Text('Hello, World!'))
 
-ft.app(target=main)
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+ft.app(main)
